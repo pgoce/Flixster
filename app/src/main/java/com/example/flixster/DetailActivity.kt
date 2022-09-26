a37a2b4acc46423bb5b34fd36ede50ee5cf9bb5a
pgoce <goce.partaloski@gmail.com>
2022-09-26 18:13:16 -0400

    Flixster Part 2 Added details to movies on whenver clicked. Added video trailers for each movie. Added a play icon overlay for 'popular movies'(7.5 rating or greater) Added rounded corners to images.

---
+++ app/src/main/java/com/example/flixster/DetailActivity.kt
@@ -0,0 +1,83 @@
+package com.example.flixster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
+
+private const val YOUTUBE_API_KEY = "AIzaSyCN35YPEEi5HiZAOCYksooMTXK7cB6hYL0"
+private const val TRAILERS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=55bb1c0339b36995645dd311ce2e7aa1"
+private const val TAG = "DetailActivity"
+class DetailActivity : YouTubeBaseActivity() {
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvOverview: TextView
+    private lateinit var ratingBar: RatingBar
+    private lateinit var ytPlayerView: YouTubePlayerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvOverview = findViewById(R.id.tvOverview)
+        ratingBar = findViewById(R.id.rbVoteAverage)
+        ytPlayerView = findViewById(R.id.player)
+
+        val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
+        tvTitle.text = movie.title
+        tvOverview.text = movie.overview
+        ratingBar.rating = movie.voteAverage.toFloat()
+
+        val client = AsyncHttpClient()
+        client.get(TRAILERS_URL.format(movie.movieId), object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.e(TAG, "onSuccess")
+                val results = json.jsonObject.getJSONArray("results")
+                if (results.length() == 0) {
+                    Log.w(TAG, "No movie trailer found")
+                    return
+                }
+                val movieTrailerJson = results.getJSONObject(0)
+                val youtubeKey = movieTrailerJson.getString("key")
+                initializeYoutube(youtubeKey)
+            }
+        })
+    }
+
+    private fun initializeYoutube(youtubeKey: String) {
+        ytPlayerView.initialize(YOUTUBE_API_KEY, object: YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                Log.i(TAG, "onInitializationSuccess")
+                player?.cueVideo(youtubeKey);
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Log.i(TAG, "onInitializationFailure")
+            }
+        })
+    }
+}
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,15 +1,20 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -26,23 +31,51 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     override fun getItemCount() = movies.size
 
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+        private val ivOverlayPlay = itemView.findViewById<ImageView>(R.id.ivOverlayPlay)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie:Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
 
             if (context.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT ) {
-                Glide.with(context).load(movie.posterImageUrl).placeholder(R.drawable.placeholderimage).into(ivPoster)
+                Glide.with(context).load(movie.posterImageUrl).placeholder(R.drawable.placeholderimage).centerCrop().transform(RoundedCorners(50)).into(ivPoster)
+                if(movie.voteAverage >= 7.5) {
+                    Glide.with(context).load(R.drawable.youtubeplay).into(ivOverlayPlay)
+                }
+                else {
+                    Glide.with(context).load(R.drawable.transparent).into(ivOverlayPlay)
+                }
             }
             else {
-                Glide.with(context).load(movie.backdropImageUrl).placeholder(R.drawable.placeholderimage).into(ivPoster)
+                Glide.with(context).load(movie.backdropImageUrl).placeholder(R.drawable.placeholderimage).centerCrop().transform(RoundedCorners(50)).into(ivPoster)
+                if(movie.voteAverage >= 7.5) {
+                    Glide.with(context).load(R.drawable.youtubeplay).into(ivOverlayPlay)
+                }
+                else{
+                    Glide.with(context).load(R.drawable.transparent).into(ivOverlayPlay)
+                }
             }
 
+
+
+        }
+
+        override fun onClick(p0: View?) {
+            val movie = movies[adapterPosition]
+//            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
